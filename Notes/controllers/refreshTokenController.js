a40e45209947ffbd56114c802a9b9582df9ca0d9
adarshsingh58 <adarshsingh58@gmail.com>
2025-04-15 21:49:59 +0530

    code refactored

--- Notes/controllers/refreshTokenController.js
+++ Notes/controllers/refreshTokenController.js
@@ -4,24 +4,33 @@ require('dotenv').config();
 
 const userDB = {
     user: require('../models/users.json'),
-    serUsers: function (data) {
+    setUsers: function (data) {
         this.user = data
     }
 }
 const handleRefreshToken = async (req, res) => {
-    const cookies = req.cookie;
-    if (!cookies?.jwt) return res.sendStatus(401);
+    const cookies = req.cookies;
+    if (!cookies?.jwt) {
+        console.log(`No JWT Refresh Token Set in Cookie`);
+        return res.status(401).json({"message": "No JWT Refresh Token Set in Cookie"});
+    }
     console.log(cookies.jwt);
     const refreshToken = cookies.jwt;
 
     const foundUser = userDB.user.find(user => user.refreshToken === refreshToken);
-    if (!foundUser) res.sendStatus(403);
+    if (!foundUser) {
+        console.log(`No user with refresh Token ${refreshToken}`);
+        return res.status(403).json({"message": "No user with this refresh Token"});
+    }
 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decode) => {
-            if (err || foundUser.username !== decode.username) return res.sendStatus(403);
+            if (err || foundUser.username !== decode.username) {
+                console.log(`User from Request refreshToken '${decode.username}' not Matching with associated user from DB '${foundUser.username}'`);
+                return res.status(403).json({"message": "User from Request refreshToken not Matching with associated user from DB"});
+            }
             const newAccessToken = jwt.sign(
                 {"username": decode.username, "roles": decode.roles},
                 process.env.ACCESS_TOKEN_SECRET,
@@ -30,7 +39,6 @@ const handleRefreshToken = async (req, res) => {
             res.json({newAccessToken});
         }
     );
-
 }
 
 module.exports = handleRefreshToken;
--- Notes/controllers/registerUser.js
+++ Notes/controllers/registerUser.js
@@ -1,25 +1,38 @@
 const userDB = {
     user: require('../models/users.json'),
-    serUsers: function (data) {
+    setUsers: function (data) {
         this.user = data
     }
 }
+const Roles = require('../models/roles.json');
 const fsPromise = require('fs').promises;
 const path = require('path');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
-    const {username, password} = req.body;
-    if (!username || !password) return res.status(400).json({'message': 'No creds given'});
+    console.log("Registering New User");
+    const {username, password, roles} = req.body;
+
+    const found = userDB.user.find(u => u.username === username);
+    if (found)
+        return res.status(400).json({"message": "User Already Exist"});
+
+    console.log("Registering New User: " + username);
+    if (!username || !password) return res.status(400).json({'message': 'Username or password Missing'});
     const hashedpwd = await bcrypt.hash(password, 10);
     console.log("hashed pass: ->   " + hashedpwd);
-    const newUsr = {'username': username, 'password': hashedpwd, 'roles': ['Admin']};
-    userDB.serUsers([...userDB.user, newUsr]);
+    const newUsr = {
+            'username': username,
+            'password': hashedpwd,
+            'roles': roles !== undefined && roles.length !== 0 ? roles : [].concat(Roles.find(u => u.RoleName === 'StandardUser').RoleName)
+
+        }
+    ;
+    userDB.setUsers([...userDB.user, newUsr]);
     await fsPromise.writeFile(
         path.join(__dirname, '..', 'models', 'users.json'), JSON.stringify(userDB.user)
     );
     res.status(500).json({'success': 'new user created'});
-
 }
 
 module.exports = handleNewUser;
