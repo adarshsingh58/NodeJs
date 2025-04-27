c11ee27a21005ad087bc5ff4be4f41e73e8a9c5b
adarshsingh58 <adarshsingh58@gmail.com>
2025-04-27 22:38:45 +0530

    added Google Oauth + RateLimiter with express Limiter and Woth Redis

--- Notes/middleware/middleware.md
+++
@@ -1,1 +0,0 @@
-https://www.youtube.com/watch?v=lY6icfhap2o
--- Notes/models/note.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose")
-
-const noteModel=new mongoose.Schema({
-    noteName:{type:String}
-});
-module.exports= mongoose.models.note || mongoose.model('note',noteModel);
